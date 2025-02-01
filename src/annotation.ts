58f6aa80a77441d18175ed09b53ab69674eda700
zeru-t <ztessema1@gmail.com>
2025-02-01 16:20:30 -0500

    update logic

--- src/annotation.ts
+++ src/annotation.ts
@@ -1,53 +1,40 @@
-import { CodeLensProvider, CodeLens, Event, EventEmitter, workspace, TextDocument, CancellationToken } from 'vscode';
+import { CodeLensProvider, CodeLens, CancellationToken, TextDocument, workspace } from 'vscode';
 
 export class AnnotationProvider implements CodeLensProvider {
 
-	private codeLenses: CodeLens[] = [];
-	private _onDidChangeCodeLenses: EventEmitter<void> = new EventEmitter<void>();
-	public readonly onDidChangeCodeLenses: Event<void> = this._onDidChangeCodeLenses.event;
-
-	constructor() {
-		workspace.onDidChangeConfiguration((_) => {
-			this._onDidChangeCodeLenses.fire();
-		});
-	}
+	constructor() {}
 
 	public async provideCodeLenses(document: TextDocument, _token: CancellationToken) {
-		let addCodeLens = false;
-		this.codeLenses = [];
+		const docText = document.getText().trim();
+		const scriptsTextMatch = /(.+"scripts":\s*{\n\s*)(.+?)(\s*})/gs.exec(docText);
 
-		const newPath = document.uri.path.replace('.json', '.annotations.json');
-		const doc = await workspace.openTextDocument(newPath);
-		const annotations = JSON.parse(doc.getText()) as Annotation;
+		if (scriptsTextMatch?.length !== 4)
+			return [];
 
-		for (let i = 0; i < document.lineCount; i++) {
-			const { text: lineText, range } = document.lineAt(i);
-			const text = lineText.trim();
-			
-			if (!text.length || text.startsWith('}')) {
-				addCodeLens = false;
-				continue;
-			}
+		const [ _, scriptsStartText, scripts ] = scriptsTextMatch;
 
-			if (addCodeLens) {
-				const regex = /"(.+)(": ".+")/g;
-				const match = regex.exec(text);
-				if (match?.length !== 3)
-					continue;
+		const annotationsFilePath = document.uri.path.replace('.json', '.annotations.json');
+		const annotationsFile = await workspace.openTextDocument(annotationsFilePath);
+		const annotations = JSON.parse(annotationsFile.getText()) as Annotation;
 
-				const description = annotations[match[1]];
-				if (!description)
-					continue;
+		const scriptsStart = scriptsStartText.split('\n').length - 1;
+		return scripts
+			.split('\n')
+			.map((script, index) => {
+				const scriptNameMatch = /"(.+)(": ".+")/g.exec(script.trim());
+				if (scriptNameMatch?.length !== 3)
+					return null;
 
-				this.codeLenses.push(new CodeLens(range, {
-					title: description,
-					command: "",
-				}));
-			}
-			if (text.includes('"scripts":'))
-				addCodeLens = true;
-		}
-		return this.codeLenses;
+				const [ _, scriptName ] = scriptNameMatch;
+				const annotation = annotations[scriptName];
+				if (!annotation)
+					return null;
+
+				const range = document.lineAt(scriptsStart + index).range;
+				const options = { title: annotation, command: "" };
+				return new CodeLens(range, options);
+			})
+			.filter(script => script !== null);
 	}
 
 	public resolveCodeLens(codeLens: CodeLens, _token: CancellationToken) {
